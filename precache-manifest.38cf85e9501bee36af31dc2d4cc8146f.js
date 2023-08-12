66a36e5667ebbd05c2a9edae529728ddf1c67d65
Fahad Alvi <fahad994@gmail.com>
2023-08-12 14:59:48 -0400

    Updates

---
+++ precache-manifest.38cf85e9501bee36af31dc2d4cc8146f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b53aa54c0e2fb25deaf91bb4db659a",
+    "url": "/Amazon-Clone/index.html"
+  },
+  {
+    "revision": "77c832559959153b8063",
+    "url": "/Amazon-Clone/static/css/main.20aff1d5.chunk.css"
+  },
+  {
+    "revision": "ad06c4304d1b4890c166",
+    "url": "/Amazon-Clone/static/js/2.2a0b2d31.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/Amazon-Clone/static/js/2.2a0b2d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77c832559959153b8063",
+    "url": "/Amazon-Clone/static/js/main.ec7066ef.chunk.js"
+  },
+  {
+    "revision": "4cb599ab17126a081400",
+    "url": "/Amazon-Clone/static/js/runtime-main.300eae63.js"
+  }
+]);
